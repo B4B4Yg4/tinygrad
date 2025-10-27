8da36233b163b22a83e861f3b66b529773b64d4b
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2025-10-27 16:15:05 +0000

    mnist file

---
+++ test/mnist.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+import requests
+import os
+import gzip
+import hashlib
+import numpy as np
+from tinygrad.tensor import Tensor
+from tqdm import trange
+
+
+# load the mnist dataset
+def fetch(url):
+
+    fp = os.path.join("/tmp", hashlib.md5(url.encode("utf-8")).hexdigest())
+
+    if os.path.isfile(fp):
+
+        with open(fp, "rb") as f:
+            data = f.read()
+
+    else:
+
+        with open(fp, "wb") as f:
+
+            data = requests.get(url).content
+            f.write(data)
+
+    return np.frombuffer(gzip.decompress(data), dtype=np.uint8).copy()
+
+
+X_train = fetch(
+    "http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz"
+)[0x10:].reshape((-1, 28, 28))
+Y_train = fetch(
+    "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz"
+)[8:]
+X_test = fetch(
+    "http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz"
+)[0x10:].reshape((-1, 28, 28))
+Y_test = fetch(
+    "http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz"
+)[8:]
+
+
+def layer_init(m, h):
+
+    ret = np.random.uniform(-1., 1., size=(m, h)) / np.sqrt(m * h)
+    return ret.astype(np.float32)
+
+
+class TinyBobNet:
+
+    def __init__(self):
+
+        self.l1 = Tensor(layer_init(784, 128))
+        self.l2 = Tensor(layer_init(128, 10))
+
+    def forward(self, x):
+
+        return x.dot(self.l1).relu().dot(self.l2).logsoftmax()
+
+
+model = TinyBobNet()
+
+lr = 0.01
+BS = 128
+losses, accuracies = [], []
+
+for i in (t := trange(1000)):
+
+    samp = np.random.randint(0, X_train.shape[0], size=(BS))
+
+    x = Tensor(layer_init(784, 128))
+    Y = Y_train[samp]
+    y = np.zeros((len(samp), 10), np.float32)
+    y[range(y.shape[0]), Y] = -1.0
+    y = Tensor(y)
+
+    # network
+    outs = model.forward(x)
+
+    # Loss function
+    loss = outs.mul(y).mean()
+    loss.backward()
+
+    cat = np.argmax(outs.data, axis=1)
+    accuracy = (cat == Y).mean()
+
+    # SGD
+    model.l1.data = model.l1.data - lr * model.l1.grad
+    model.l2.data = model.l2.data - lr * model.l2.grad
+
+    # printing
+    loss = loss.data
+    losses.append(loss)
+    accuracies.append(accuracy)
+    t.set_description("loss %.2f accuracy %.2f" % (loss, accuracy))
+
+
+def numpy_eval():
+
+    Y_test_preds_out = model.forward(Tensor(X_test.reshape((-1, 28 * 28))))
+    Y_test_preds = np.argmax(Y_test_preds_out.data, axis=1)
+
+    return (Y_test == Y_test_preds).mean()
+
+
+accuracy = numpy_eval()
+print("Test set accuracy is %f" % accuracy)
+assert accuracy > 0.95
